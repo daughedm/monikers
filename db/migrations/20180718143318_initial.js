20f5cb12646d727d6a774b44207ead02a036d0da
David Daugherty <david.m.daugherty@gmail.com>
2018-07-18 16:04:12 -0600

    Update migrations file

--- db/migrations/20180718143318_initial.js
+++ db/migrations/20180718143318_initial.js
@@ -1,20 +1,18 @@
-exports.up = function (knex, Promise) {
+exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('cards', function (table) {
+    knex.schema.createTable('cards', function(table) {
       table.increments('id').primary();
       table.integer('user');
       table.string('name');
-      table.string('summary');
+      table.string('description', 500);
       table.integer('pointValue');
       table.string('category');
 
-      table.timestamps(true, true)
+      table.timestamps(true, true);
     })
-  ])
+  ]);
 };
 
-exports.down = function (knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable('cards')
-  ])
+exports.down = function(knex, Promise) {
+  return Promise.all([knex.schema.dropTable('cards')]);
 };
