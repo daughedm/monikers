c75079415afb16714adb38ab6d20ffe4f9c7d550
Krista Handel <k.handel66@gmail.com>
2018-07-19 15:29:17 -0600

    Change teamNames import Clean syntax

--- src/components/Setup/Setup.js
+++ src/components/Setup/Setup.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import teamNames from '../../actions';
 import './Setup.css';
 
 export class Setup extends Component {
@@ -23,8 +23,6 @@ export class Setup extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-
-    console.log(this.state.teamName);
 
     this.props.teamNames(this.state.teamName);
   };
@@ -48,7 +46,7 @@ export class Setup extends Component {
 export const mapStateToProps = state => ({});
 
 export const mapDispatchToProps = dispatch => ({
-  teamNames: teamName => dispatch(actions.teamNames(teamName))
+  teamNames: teamName => dispatch(teamNames(teamName))
 });
 
 Setup.propTypes = {};
