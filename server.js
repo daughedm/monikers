c36b610ceab442c22b873e4b31a6015737f6f649
hmmChase <hmmchase@gmail.com>
2018-07-19 15:26:10 -0600

    Change port number

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Monikers';
 
 app.use(bodyParser.json());
