bcb94c5f230ab17ad16c7274466a8753ac29e426
Krista Handel <k.handel66@gmail.com>
2018-07-19 17:09:42 -0600

    Add num players reducer

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,9 +1,16 @@
-const intialState = [];
-
-export const teamNames = (state = intialState, action) => {
+export const teamNames = (state = [], action) => {
   switch (action.type) {
     case 'TEAM_NAMES':
       return [...state, action.teamName];
+    default:
+      return state;
+  }
+};
+
+export const numOfPlayers = (state = -1, action) => {
+  switch (action.type) {
+    case 'NUM_OF_PLAYERS':
+      return [action.number];
     default:
       return state;
   }
