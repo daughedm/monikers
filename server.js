9dd2b4cb8fde90ab3e87122b140d1abf32bc3149
hmmChase <hmmchase@gmail.com>
2018-07-19 14:48:38 -0600

    Fix formatting

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-const environment = process.env.NODE_ENV || "development";
-const configuration = require("./knexfile")[environment];
-const database = require("knex")(configuration);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
-app.set("port", process.env.PORT || 3000);
-app.locals.title = "Monikers";
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Monikers';
 
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/api/v1/cards", (request, response) => {
-  database("monikers")
+app.get('/api/v1/cards', (request, response) => {
+  database('monikers')
     .select()
     .then(cards => {
       response.status(200).json(cards);
@@ -25,10 +25,10 @@ app.get("/api/v1/cards", (request, response) => {
     });
 });
 
-app.post("/api/v1/cards", (request, response) => {
+app.post('/api/v1/cards', (request, response) => {
   let newCard = request.body;
 
-  for (let requiredParameter of ["name", "summary", "pointValue", "category"]) {
+  for (let requiredParameter of ['name', 'summary', 'pointValue', 'category']) {
     if (!newCard[requiredParameter]) {
       return response.status(422).send({
         error: `You are missing a ${requiredParameter}`
@@ -36,8 +36,8 @@ app.post("/api/v1/cards", (request, response) => {
     }
   }
 
-  database("monikers")
-    .insert(newCard, "id")
+  database('monikers')
+    .insert(newCard, 'id')
     .then(cardId => {
       return response.status(201).json({
         id: cardId[0]
@@ -50,20 +50,20 @@ app.post("/api/v1/cards", (request, response) => {
     });
 });
 
-app.delete("/api/v1/cards/:id", (request, response) => {
+app.delete('/api/v1/cards/:id', (request, response) => {
   let id = request.params.id;
 
-  database("monikers")
-    .where("id", id)
+  database('monikers')
+    .where('id', id)
     .del()
     .then(res => {
       if (res === 1) {
         return response.status(204).json({
-          message: "Success"
+          message: 'Success'
         });
       } else {
         return response.status(404).json({
-          message: "This item does not exist"
+          message: 'This item does not exist'
         });
       }
     })
@@ -74,16 +74,11 @@ app.delete("/api/v1/cards/:id", (request, response) => {
     });
 });
 
-app.put("/api/v1/cards/:id", (request, response) => {
-  const {
-    id
-  } = request.params;
-  const {
-    title,
-    packed
-  } = request.body;
-  database("monikers")
-    .where("id", id)
+app.put('/api/v1/cards/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, packed } = request.body;
+  database('monikers')
+    .where('id', id)
     .update({
       title,
       packed
@@ -94,8 +89,8 @@ app.put("/api/v1/cards/:id", (request, response) => {
     .catch(error => response.status(400).send(error));
 });
 
-app.listen(app.get("port"), () => {
-  console.log(`${app.locals.title} is running on ${app.get("port")}`);
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`);
 });
 
 module.exports = {
