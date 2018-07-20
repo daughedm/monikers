814d2028aad32a18af01b8dfebaf5dc984979524
Krista Handel <k.handel66@gmail.com>
2018-07-20 11:38:26 -0600

    Remove authentication functionality Not needed at this time, logic saved on another branch when needed

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import * as actions from '../../actions/index';
-import Setup from '../../components/Setup/Setup';
+import Setup from '../../components/Setup/Setup'
 import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Security, ImplicitCallback } from '@okta/okta-react';
-import Home from '../../components/Home/Home';
-
-const config = {
-  issuer: 'https://dev-458733.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '0oafrod8i3NBZ6qCm0h7'
-};
 
 export class App extends Component {
   componentDidMount() {}
@@ -21,14 +12,7 @@ export class App extends Component {
   render() {
     return (
       <Router>
-        <Security
-          issuer={config.issuer}
-          client_id={config.client_id}
-          redirect_uri={config.redirect_uri}
-        >
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/implicit/callback" component={ImplicitCallback} />
-        </Security>
+          <Route path="/" exact={true} component={Setup} />
       </Router>
     );
   }
