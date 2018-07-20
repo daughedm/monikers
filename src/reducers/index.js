985d6e81128d7a827e58b2b82c2e440868c27acb
hmmChase <hmmchase@gmail.com>
2018-07-20 14:06:59 -0600

    Add action/reducer to add card to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { teamNames, numPlayers } from './gameReducer';
+import { cards } from './cardReducer';
 
 const rootReducer = combineReducers({
   teamNames: teamNames,
-  numPlayers: numPlayers
+  numPlayers: numPlayers,
+  cards: cards
 });
 
 export default rootReducer;
