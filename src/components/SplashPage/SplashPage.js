1061057729f0f5ef79713e1d005ac25fba9c413a
Krista Handel <k.handel66@gmail.com>
2018-07-21 11:20:02 -0600

    Add linter script

---
+++ src/components/SplashPage/SplashPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SplashPage = () => {
+  return (
+    <form>
+      <h1>Monikers</h1>
+      <input type="submit" value="Start" />
+      <input type="submit" value="Instructions" />
+    </form>
+  );
+};
+
+export default SplashPage;
