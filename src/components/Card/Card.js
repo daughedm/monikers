9ea5431d0424a427b406e75ae9e316442eb80b52
Krista Handel <k.handel66@gmail.com>
2018-07-23 12:59:01 -0600

    Begin card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class Card extends Component() {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="card-container">
+        <h1 className="card-title">{this.props.activeCards[0].name}</h1>
+
+        <p className="description">{this.props.activeCards[0].description}</p>
+        <div className="dashed-line" ></div>
+        <h3 className="category">{this.props.activeCards[0].category}</h3>
+        {/* this divs colors will change based on category */}
+        <div className="circle">
+          <h1 className="points">{this.props.activeCards[0].pointValue}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
