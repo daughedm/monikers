16b01177de06337f563ab6500a890416526ebb55
Chase <hmmChase@users.noreply.github.com>
2018-07-25 17:11:27 -0600

    Add functionality to store game info in IDB

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -11,16 +11,76 @@ import Next from '../../components/Next/Next';
 import Instructions from '../../components/Instructions/Instructions';
 import Loading from '../../components/Loading/Loading';
 import indexedDB from '../../indexedBD';
-import { addCard } from '../../actions/gameActions';
+import {
+  addCard,
+  addTeamNames,
+  numOfCards,
+  updateActiveCards
+} from '../../actions/gameActions';
 import * as api from '../../api/api';
 
 import './App.css';
 
-
 export class App extends Component {
   async componentDidMount() {
     await this.cardsPGtoIDB();
+    this.loadInProgressGameInfo();
   }
+
+  loadInProgressGameInfo = async () => {
+    this.loadTeamNames();
+    this.loadNumCards();
+    this.loadActiveCards();
+
+    // const teamOneID = await indexedDB.teams
+    //   .where('team')
+    //   .equals(1)
+    //   .primaryKeys();
+    // // get teamOne Object
+    // const teamOneObj = await indexedDB.teams.get(teamOneID[0]);
+    // // get teamOne name
+    // const teamOneName = teamOneObj.name;
+    // const teamOneID = await indexedDB.teams
+    //   .where('team')
+    //   .equals(1)
+    //   .primaryKeys();
+    // // get teamOne Object
+    // const teamOneObj = await indexedDB.teams.get(teamOneID[0]);
+    // // get teamOne name
+    // const teamOneName = teamOneObj.name;
+  };
+
+  loadTeamNames = async () => {
+    const teamsIDB = await indexedDB.teamNames.toArray();
+
+    if (teamsIDB.length) {
+      teamsIDB.forEach(team => {
+        this.props.addTeamNames(team.name);
+      });
+    } else {
+      this.props.history.push('/');
+    }
+  };
+
+  loadNumCards = async () => {
+    const numCardsIDB = await indexedDB.numCards.toArray();
+
+    if (numCardsIDB.length) {
+      this.props.numOfCards(parseInt(numCardsIDB[0].num));
+    } else {
+      this.props.history.push('/');
+    }
+  };
+
+  loadActiveCards = async () => {
+    const activeCardsIDB = await indexedDB.activeCards.toArray();
+
+    if (activeCardsIDB.length) {
+      this.props.updateActiveCards(activeCardsIDB);
+    } else {
+      this.props.history.push('/');
+    }
+  };
 
   cardsPGtoIDB = async () => {
     const cardsPG = await api.getCards();
@@ -46,7 +106,10 @@ export class App extends Component {
 export const mapStateToProps = state => ({});
 
 export const mapDispatchToProps = dispatch => ({
-  addCard: card => dispatch(addCard(card))
+  addCard: card => dispatch(addCard(card)),
+  addTeamNames: teamName => dispatch(addTeamNames(teamName)),
+  numOfCards: number => dispatch(numOfCards(number)),
+  updateActiveCards: cards => dispatch(updateActiveCards(cards))
 });
 
 App.propTypes = {};
