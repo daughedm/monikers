6629777e8f54feeb9a24cdf6a432c2f518ec213d
Krista Handel <k.handel66@gmail.com>
2018-07-25 09:39:27 -0600

    Remove commented out code

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -17,17 +17,7 @@ export class Play extends Component {
     };
   }
 
-  componentDidMount() {
-    // this.countDown();
-  }
-
-  // componentWillReceiveProps(nextProps) {
-  //   if (this.props.teamTimer !== nextProps.teamTimer) {
-  //     this.setState({
-  //       state: nextProps.teamTimer
-  //     });
-  //   }
-  // }
+  componentDidMount() {}
 
   handleGotIt = e => {
     e.preventDefault();
@@ -43,20 +33,6 @@ export class Play extends Component {
     this.props.updateActiveCards(newCards);
   };
 
-  // countDown = () => {
-  //   let count = 60,
-  //     timer = setInterval(() => {
-  //       count--;
-  //       if (count === 0) {
-  //         this.props.updateTeamTimer('stopped')
-  //         this.props.currTeam === this.props.teamNames[0] 
-  //           ? this.props.currentTeam(this.props.teamNames[1]) 
-  //           : this.props.currentTeam(this.props.teamNames[0]);
-  //         clearInterval(timer);
-  //       }
-  //     }, 1000);
-  // };
-
   handleSkipped = e => {
     e.preventDefault();
 
@@ -67,7 +43,7 @@ export class Play extends Component {
   };
 
   render() {
-    
+
     if (this.props.teamTimer === 'stopped') {
       return <Next />;
     } else if (this.props.activeCards.length === 0){
