5854a627f1d03b2e4b7f2692ce41cf7b34999617
Krista Handel <k.handel66@gmail.com>
2018-07-25 17:40:10 -0600

    Break out car actions into own file

---
+++ src/actions/cardActions.js
@@ -0,0 +1,19 @@
+export const numOfCards = number => ({
+  type: 'NUM_OF_CARDS',
+  number
+});
+
+export const addCard = card => ({
+  type: 'ADD_CARD',
+  card
+});
+
+export const updateActiveCards = cards => ({
+  type: 'UPDATE_ACTIVE_CARDS',
+  cards
+})
+
+export const discardedCards = card => ({
+  type: 'DISCARDED_CARDS',
+  card
+});
