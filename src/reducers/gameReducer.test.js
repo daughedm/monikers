a5e933b2f6d237c2dc1beda32f8a29f37c089069
Chase <hmmChase@users.noreply.github.com>
2018-07-25 17:12:38 -0600

    Update action type

--- src/reducers/gameReducer.test.js
+++ src/reducers/gameReducer.test.js
@@ -1,16 +1,16 @@
 import { teamNames } from './gameReducer';
 
-describe("Game reducers", () => {
-  describe("teamNames", () => {
-    it("should return default state when no action passed", () => {
+describe('Game reducers', () => {
+  describe('teamNames', () => {
+    it('should return default state when no action passed', () => {
       expect(teamNames([], {})).toEqual([]);
     });
 
-    it("should return an array of team names", () => {
+    it('should return an array of team names', () => {
       const expected = ['team one'];
 
       const mockAction = {
-        type: 'TEAM_NAMES',
+        type: 'ADD_TEAM_NAME',
         teamName: 'team one'
       };
 
