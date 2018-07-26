e29b9022ba7c16456d4cecfb9dc4ac8766f6b519
Chase <hmmChase@users.noreply.github.com>
2018-07-26 16:55:53 -0600

    Remove unneeded components

--- src/components/Ready/Ready.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Ready from './Ready';
-
-it('renders without crashing', () => {
-  expect(true).toEqual(true)
-});
--- src/components/Start/Start.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Start from './Start';
-
-it('renders without crashing', () => {
-  expect(true).toEqual(true)
-});
