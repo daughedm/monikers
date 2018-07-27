6b6ea2e992a2571f616c568f4dcc8ef4676b0777
Krista Handel <k.handel66@gmail.com>
2018-07-26 22:29:23 -0600

    Add game finish component

---
+++ src/components/Finish/Finish.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class Finish extends Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    const { teamNames, teamOneScore, teamTwoScore } = this.props;
+    const winningTeam = teamOneScore > teamTwoScore ? 0 : 1;
+    return (
+      <div className="background-monikers">
+        <div className="round-transition">
+          <h2 className="round-headline">Team {teamNames[winningTeam]} wins!</h2>
+          <div className="dashed-line" />
+        </div>
+      </div>
+    );
+  }
+};
+
+export const mapStateToProps = state => ({
+  teamNames: state.teamNames,
+  teamOneScore: state.teamOneScore,
+  teamTwoScore: state.teamTwoScore
+});
+
+export default connect(mapStateToProps)(Finish);
