6f275a465d7b166854f8e9b2c27813a11153dd77
Krista Handel <k.handel66@gmail.com>
2018-07-26 23:24:16 -0600

    Add new game button and functionality

--- src/components/Finish/Finish.js
+++ src/components/Finish/Finish.js
@@ -6,6 +6,12 @@ export class Finish extends Component {
     super();
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.history.push('/setup');
+  }
+
   render() {
     const { teamNames, teamOneScore, teamTwoScore } = this.props;
     const winningTeam = teamOneScore > teamTwoScore ? 0 : 1;
@@ -14,6 +20,9 @@ export class Finish extends Component {
         <div className="round-transition">
           <h2 className="round-headline">Team {teamNames[winningTeam]} wins!</h2>
           <div className="dashed-line" />
+          <button className="start-button" type="submit" onClick={this.handleSubmit}>
+            NEW GAME
+          </button>
         </div>
       </div>
     );
