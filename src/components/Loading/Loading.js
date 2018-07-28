33cfb787486e9ece651b93a7ec00ffcb0be39cfd
Chase <hmmChase@users.noreply.github.com>
2018-07-28 12:29:11 -0600

    Convert to stateless component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
 import logolockup from '../../assets/Monikers_logo_lockup-02.svg';
 import loadingGif from '../../assets/loading.svg';
 import './Loading.css';
 
-export class Loading extends Component {
-  componentDidMount() {}
+const Loading = () => {
+  return (
+    <div className="loading">
+      <img className="logo-lockup" src={logolockup} alt="logolockup" />
+      <img className="loading-gif" src={loadingGif} alt="loadingGif" />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="loading">
-        <img className="logo-lockup" src={logolockup} alt="logolockup" />
-        <img className="loading-gif" src={loadingGif} alt="loadingGif" />
-      </div>
-    );
-  }
-}
-
-export const mapStateToProps = state => ({});
-
-export const mapDispatchToProps = dispatch => ({});
-
-Loading.propTypes = {};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Loading);
+export default Loading;
