57d25bcdf12aac2d4e167466f149315208520682
Chase <hmmChase@users.noreply.github.com>
2018-07-28 12:17:59 -0600

    Fix radix

--- src/components/Setup/Setup.js
+++ src/components/Setup/Setup.js
@@ -65,7 +65,7 @@ export class Setup extends Component {
 
     addTeamNames(teamOne);
     addTeamNames(teamTwo);
-    numOfCards(parseInt(numCards), 10);
+    numOfCards(parseInt(numCards, 10));
     currentTeam(teamOne);
   };
 
