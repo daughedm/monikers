06a14ffc194cb3f8bc6c564db4df7b032531d44c
hmmChase <hmmchase@gmail.com>
2018-07-30 10:48:00 -0600

    Add card tests

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,7 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Card from './Card';
+import { shallow } from 'enzyme';
+import { Card, mapStateToProps } from './Card';
+import * as mockData from '../../__mocks__/mockData';
 
-it('renders without crashing', () => {
-  expect(true).toEqual(true)
+describe('Card', () => {
+  let card;
+  let mockProps;
+
+  beforeEach(() => {
+    mockProps = {
+      activeCards: mockData.activeCards
+    };
+
+    card = shallow(<Card {...mockProps} />);
+  });
+
+  it('matches the snapshot', () => {
+    expect(card).toMatchSnapshot();
+  });
+
+  it('matches the snapshot if point value is 1', () => {
+    mockProps.activeCards.pointValue = 1;
+    card = shallow(<Card {...mockProps} />);
+    expect(card).toMatchSnapshot();
+  });
+
+  it('matches the snapshot if point value is 2', () => {
+    mockProps.activeCards[0].pointValue = 2;
+    card = shallow(<Card {...mockProps} />);
+    expect(card).toMatchSnapshot();
+  });
+
+  it('matches the snapshot if point value is 3', () => {
+    mockProps.activeCards.pointValue = 3;
+    card = shallow(<Card {...mockProps} />);
+    expect(card).toMatchSnapshot();
+  });
+
+  it('matches the snapshot if point value is 4', () => {
+    mockProps.activeCards.pointValue = 4;
+    card = shallow(<Card {...mockProps} />);
+    expect(card).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('maps state properties to props', () => {
+      const mockState = {
+        activeCards: mockData.activeCards
+      };
+      const expected = {
+        activeCards: mockData.activeCards
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
 });
