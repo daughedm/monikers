3a93ddb7e7a0218b28d9c6af0206a4fc53c33057
hmmChase <hmmchase@gmail.com>
2018-07-30 14:00:17 -0600

    Seperate makeFetch into own file

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,4 @@
-export const makeFetch = async (url, options) => {
-  try {
-    const response = await fetch(url, options);
-
-    if (!response.ok) {
-      throw new Error(`${response.status}`);
-    }
-    return await response.json();
-  } catch (error) {
-    throw new Error(`Network request failed. (error: ${error.message})`);
-  }
-};
+import { makeFetch } from './fetch';
 
 export const getCards = async () => {
   const url = 'http://localhost:3001/api/v1/cards';
