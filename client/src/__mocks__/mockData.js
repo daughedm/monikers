cf2b5e906fef52671826a54b4f2d46f93f797a89
hmmChase <hmmchase@gmail.com>
2018-07-31 13:42:23 -0600

    Add mock discardedCards

--- client/src/__mocks__/mockData.js
+++ client/src/__mocks__/mockData.js
@@ -9,3 +9,37 @@ export const activeCard = {
 };
 
 export const activeCards = [activeCard];
+
+export const discardedCards = [
+  {
+    name: 'Ebola',
+    description:
+      'A virus that causes hemorrhagic fever, typically acquired by contact with an infected monkey, fruit bat, or person. Its most severe symptoms can include bleeding from the eyes, nose, ears, mouth, and rectum. There is no specific treatment for the disease.',
+    category: 'ET CETERA',
+    pointValue: 2
+  },
+  {
+    name: 'Kobayashi',
+    description:
+      "A Japanese competitive eater who shocked the world in 2001 by eating 50 hot dogs and buns (HDB) in 12 minutes at Nathan's Hot Dog Eating Contest, doubling the previous record. He once lost a hot dog eating contest (no buns) to a 1089 lb. Kodiak bear (31-50).",
+    category: 'CELEBRITY',
+    pointValue: 2
+  }
+];
+
+export const cards = [
+  {
+    name: 'Oscar Pistorius',
+    description:
+      'Sometimes known as “Blade Runner" or “the fastest man on no legs," this sprinter became the first double leg amputee to participate in the Olympics. He was subsequently charged with the murder of his girlfriend, but maintains that he confused her for an intruder.',
+    category: 'CELEBRITY',
+    pointValue: 2
+  },
+  {
+    name: 'Elian Gonzalez',
+    description:
+      "A Cuban boy who was the subject of an international incident when his relatives attempted to keep him in the US against his father's wishes that he return to Cuba. A famous image from the event shows an armed border agent discovering the boy cowering in a closet.",
+    category: 'HISTORICAL FIGURE',
+    pointValue: 2
+  }
+];
