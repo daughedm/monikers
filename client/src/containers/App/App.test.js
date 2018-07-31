0bd6800d9a13def301477272b675156400514d7a
hmmChase <hmmchase@gmail.com>
2018-07-31 08:42:32 -0600

    Add App tests

--- client/src/containers/App/App.test.js
+++ client/src/containers/App/App.test.js
@@ -1,9 +1,52 @@
 /* eslint-disable max-len */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import { shallow } from 'enzyme';
+import App from './app';
+import * as api from '../../api/api';
+import * as mockData from '../../__mocks__/mockData';
+jest.mock('../../api/api.js');
+import indexedDB from '../../indexedBD';
+jest.mock('../../indexedBD.js');
 
-it('renders without crashing', () => {
-  expect(true).toEqual(true);
+describe('app', () => {
+  let app;
+  let mockProps;
+
+  beforeEach(() => {
+    mockProps = {};
+    app = shallow(<App {...mockProps} />, { disableLifecycleMethods: true });
+  });
+
+  it('matches the snapshot', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  describe('componentDidMount', () => {
+    it('calls cardsPGtoIDB', async () => {
+      const cardsPGtoIDB = (app.instance().cardsPGtoIDB = jest.fn());
+
+      await app.instance().componentDidMount();
+
+      expect(cardsPGtoIDB).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('cardsPGtoIDB', () => {
+    it('calls getCards if navigator.onLine is true', async () => {
+      await app.instance().cardsPGtoIDB();
+
+      expect(api.getCards).toHaveBeenCalledTimes(1);
+    });
+
+    it.skip('calls indexedDB', async () => {
+      const indexedDB = { allCards: { clear: jest.fn() } };
+
+      await app.instance().cardsPGtoIDB();
+
+      expect(indexedDB.allCards.clear).toHaveBeenCalledTimes(1);
+
+      expect();
+    });
+  });
 });
