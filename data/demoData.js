fc74042780e4cf7a5003408a4f2201795a4db57a
Krista Handel <k.handel66@gmail.com>
2018-08-14 16:11:04 -0600

    Add cards data for demo

---
+++ data/demoData.js
@@ -0,0 +1,74 @@
+/* eslint-disable */
+
+module.exports = [
+  {
+    name: 'Tigger',
+    description:
+      'A fictional tiger with a springy tail and a love of bouncing. He lives with Winnie-the-Pooh in the Hundred Acre Wood. While he often overestimates his own abilities, his boast that never gets lost seems to be true.',
+    category: 'FICTIONAL CHARACTER',
+    pointValue: 1
+  },
+  {
+    name: 'MacGyver',
+    description:
+      'The title character from the 80s TV show about a secret agent who makes clever little devices out of whatever\'s around. These include a defibrulator (candle sticks, microphone wire, rubber mat) and a catapult (branches. rock, twine, rosary beads).',
+    category: 'FICTIONAL CHARACTER',
+    pointValue: 1
+  },
+  {
+    name: 'Patti Mayonnaise',
+    description:
+      'The yellow-haired, polka dot-shirted secret love of the cartoon character Doug. She lives with her wheelchair bound father. Her mother died in the same car accident that paralyzed her father. She is an excellent beet-ball player and gets rude when she is hungry.',
+    category: 'FICTIONAL CHARACTER',
+    pointValue: 3
+  },
+  {
+    name: 'Soylent',
+    description:
+      'A meal replacement product, which "meets all nutritional requirements for an average adult." Its tagline is, inexplicably, "Free Your Body" and its name is derived from a 1973 science fiction film, where the green product in question was actually made from people.',
+    category: 'ET CETERA',
+    pointValue: 3
+  },
+  {
+    name: 'A dingo',
+    description:
+      'A free-ranging dog found in Australia. In 1980, one killed the baby of Lindy Chamberlain. Ms. Chamberlin\'s statement, that the animal "ate my baby," was popularized by Meryl Streep in the 1988 film Evil Angels. The line remains widely parodied to this day.',
+    category: 'ET CETERA',
+    pointValue: 2
+  },
+  {
+    name: 'All the single ladies',
+    description:
+      'Every woman who is either unmarried or not involved in a long-term relationship, as described in Beyoncé\'s 2008 song. In the song, she laments the inability of men to propose marriage, and suggests that "if you like it then you should put a ring on it."',
+    category: 'ET CETERA',
+    pointValue: 1
+  },
+  {
+    name: 'Sad Keanu',
+    description:
+      'A 2010 meme based on a photo of the actor known for his starring roles in Speed. Point Break. The Matrix. and other blockbusters. He is shown sitting on a bench by himself, eating a sandwich, and looking forlorn. The action figure of this comes in two sizes: Teeny and Little.',
+    category: 'CELEBRITY',
+    pointValue: 2
+  },
+  {
+    name: 'Banksy',
+    description:
+      'The pseudonym of a mysterious UK graffiti artist. His work uses stencils to depict satirical characters, often interacting with the environment. Though nominated for an Academy Award as a documentary, his film Exit Through the Gift Shop is now widely accepted as a hoax.',
+    category: 'CELEBRITY',
+    pointValue: 2
+  },
+  {
+    name: 'Kindergarten Cop',
+    description:
+      'The title character from a 1990 comedy about a detective who goes undercover at an elementary school and discovers his love of teaching. Played by Arnold Schwarzenegger, the film is famous for the line "It‘s not a tumor!" which he delivers in his thick Austrian accent.',
+    category: 'FICTIONAL CHARACTER',
+    pointValue: 2
+  },
+  {
+    name: 'Guy Fieri\'s Donkey Sauce',
+    description:
+      'The signature mayonnaise-based, pale orange condiment invented by a famous cooking personality known for his frosted tips and goatee. He claims to have coined the term while working in the kitchen of a Carnival Cruise ship.',
+    category: 'CELEBRITY',
+    pointValue: 3
+  }
+]
